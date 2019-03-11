6e75dde65436b799db5dfae96ed245ea253da77c
Dmitry Maksimov <dmtmax@gmail.com>
2019-03-11 22:21:47 +0100

    start editor after creating a zettel

--- cmd/zk.go
+++ cmd/zk.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 	"text/template"
 	"time"
@@ -20,9 +21,19 @@ var zk = &cobra.Command{
 
 		return nil
 	},
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		title := strings.Join(args, "_")
-		createZettelFromTemplate(title)
+
+		filename, err := createZettelFromTemplate(title)
+		if err != nil {
+			return err
+		}
+
+		if err = exec.Command("code", filename).Run(); err != nil {
+			return errors.Wrap(err, "can't start editor")
+		}
+
+		return nil
 	},
 }
 
@@ -31,28 +42,26 @@ type zettelTemplate struct {
 	Title     string
 }
 
-func createZettelFromTemplate(title string) error {
+func createZettelFromTemplate(title string) (string, error) {
 	timestamp := currentTimestamp()
 	filename := fmt.Sprintf("%[1]s-%[2]s.md", timestamp, strings.ToLower(title))
 
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0755)
 	if err != nil {
-		return errors.Wrap(err, "can't create zettel file")
+		return "", errors.Wrap(err, "can't create zettel file")
 	}
 	defer f.Close()
 
 	t, err := template.ParseFiles("templates/zettel.md")
 	if err != nil {
-		return errors.Wrap(err, "can't open zettel template")
+		return "", errors.Wrap(err, "can't open zettel template")
 	}
 
 	if err = t.Execute(f, zettelTemplate{timestamp, title}); err != nil {
-		return errors.Wrap(err, "can't write template to the zettel file")
+		return "", errors.Wrap(err, "can't write template to the zettel file")
 	}
 
-	fmt.Println(filename)
-
-	return nil
+	return filename, err
 }
 
 func currentTimestamp() string {
